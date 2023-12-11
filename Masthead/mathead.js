806941f47f8207e97d35725e61e7ce20c059f4b5
dungcvvc <dung.can@netlink.vn>
2023-12-11 16:06:50 +0700

    mathead.js

---
+++ Masthead/mathead.js
@@ -0,0 +1,20 @@
+var body = document.querySelector("body");
+
+var cssMathead = document.createElement("link");
+cssMathead.href =
+  "https://cdn.jsdelivr.net/gh/netlink-player/campain@master/Masthead/masthead.css";
+cssMathead.rel = "stylesheet";
+body.appendChild(cssMathead);
+
+var classMathead = document.createElement("div");
+classMathead.class = "masthead";
+var aMathead = document.createElement("a");
+aMathead.href = "https://netlink.vn/";
+var imgMathead = document.createElement("img");
+imgMathead.src =
+  "https://cdn.jsdelivr.net/gh/netlink-player/campain@master/Masthead/video_netlink-cut.gif";
+imgMathead.alt = "masthead";
+aMathead.appendChild(imgMathead);
+classMathead.appendChild(aMathead);
+
+body.appendChild(classMathead);
